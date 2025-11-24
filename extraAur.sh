bab15fac36e568baaa0e0c7caf96fa8e51eb7152
barnabedikartola <barnabedikartola@gmail.com>
2025-11-24 18:12:51 -0300

    add extraAur

---
+++ extraAur.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+pkgbuildFile=$(find -type f -name PKGBUILD)
+
+cd biglinux-zsh-config/
+rm -rf !("PKGBUILD"|"biglinux-zsh-config.install")
+rm -rf .*
+
+# # Add ao depends ao pkgbuild
+# sed -i "/\bdepends=/s/(/\('xpto\' 'xpto3\' 'xpto2\' /" $pkgbuildFile
+# sed -i "/makedepends=/s/(/\('xpto\' 'xpto2\' 'xpto3\' /" $pkgbuildFile
+# sed -i '/\bdepends=/!b; :a; n; a\    '\'xpto\' $pkgbuildFile
+
+# # Remove depends do pkgbuild
+# sed -i "/\bdepends=/s/'xpto' //" $pkgbuildFile
+# sed -i "/makedepends=/s/'xpto' //" $pkgbuildFile
+
+# # troca xpto por otpx
+# sed -i '/\bdepends=/s/xpto/otpx/' $pkgbuildFile
+# sed -i '/makedepends=/s/xpto/otpx/' $pkgbuildFile
+
+# # Apagar bloco pkgver
+# sed -i '/pkgver()/,/^$/d' $pkgbuildFile
+
+# Cancelar workflow
+# cancel () {
+#   echo "Sem atualiza de versão"
+#   echo "SAINDO...."
+#   curl -s -H "Authorization: token ${{ github.token }}" \
+#   -X POST "https://api.github.com/repos/${{ github.repository }}/actions/runs/${{ github.run_id }}/cancel"
+#   sleep infinity
+# }
+# cancel
